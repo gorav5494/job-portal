5ef6ddbebfbc983eb3b3195c438c179ca6542c0a
goravved <goravkumarved4444@gmail.com>
2024-05-21 19:36:03 +0530

    forgot password

--- backend/routes/applyjobRoutes.js
+++ backend/routes/applyjobRoutes.js
@@ -4,5 +4,7 @@ const applicationfill = require("../controller/applyJobController");
 const router = express.Router();
 
 router.route("/fillform").post(applicationfill);
+// router.route("/updatedetails").post(applicationfill);
+// router.route("/viewdetails").get(applicationfill);
 
 module.exports = router;
